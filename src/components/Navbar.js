7ce4a372475f76c5ebc388d51cedc6de0466cd64
Franco <FrancoAguirre644@gmail.com>
2020-08-07 12:14:48 -0300

    Estructura del proyecto

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../logo.svg'
+
+export const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <a className="navbar-brand" href="#" ><img src={logo} width="40" height="40" alt="React" className=" d-inline-block align-top" />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+        </div>
+    </nav >
+)
